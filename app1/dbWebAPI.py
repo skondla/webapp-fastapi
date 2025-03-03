bf461d989b0ffd3ff81b9e2755b021326b5699d9
skondla <kondlawork@gmail.com>
2025-03-03 18:48:15 -0500

    Updated lib package reference to dbWebAPI.py and certs path for TLS connection

--- app1/dbWebAPI.py
+++ app1/dbWebAPI.py
@@ -2,11 +2,12 @@
 #Author: skondla@me.com
 #purpose: Build a simple python WebApp & REST API with FastAPI to call database service requests
 #coding=utf-8
-#Run: uvicorn dbWebAPI:app --reload --host 0.0.0.0 --port 8000 --reload --ssl-keyfile=key.pem --ssl-certfile=cert.pem
+#Run: uvicorn dbWebAPI:app --reload --host 0.0.0.0 --port 8000 --reload --ssl-keyfile=cert/key.pem --ssl-certfile=cert/certificate.pem
 from fastapi import FastAPI, Form, Request, HTTPException, Depends
 from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi.templating import Jinja2Templates
-from rdsAdmin import RDSDescribe, RDSCreate, RDSDelete
+# from fastapi.templating import Jinja2Templates
+from starlette.templating import Jinja2Templates  # ✅ Correct
+from lib.rdsAdmin import RDSDescribe, RDSCreate, RDSDelete
 import datetime
 import yaml
 import ssl
